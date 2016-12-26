f3eed147a02c9a4ddd0923fba4a6b8c4e4b59a6f
CurryHan <392463168@qq.com>
2016-12-26 11:56:50 +0800

    fix bug

--- src/SensingCloud/Controllers/WeChatController.cs
+++ src/SensingCloud/Controllers/WeChatController.cs
@@ -48,28 +48,39 @@ namespace SensingCloud.Controllers
         {
             IntroductionViewModel model = new IntroductionViewModel();
             var list1 = db.Menus.Find(1);
-            model.Url1 = list1 == null ? "" : list1.Media == null ? "" : list1.Media.Url;
+            model.Url1 = list1 == null ? "" : list1.Media == null ? "" : GetCanReturnUrl(list1.Media.Url);
 
             var list2 = db.Menus.Find(2);
-            model.Url2 = list2 == null ? "" : list2.Media == null ? "" : list2.Media.Url;
+            model.Url2 = list2 == null ? "" : list2.Media == null ? "" : GetCanReturnUrl(list2.Media.Url);
 
             var list3 = db.Menus.Find(3);
-            model.Url3 = list3 == null ? "" : list3.Media == null ? "" : list3.Media.Url;
+            model.Url3 = list3 == null ? "" : list3.Media == null ? "" : GetCanReturnUrl(list3.Media.Url);
 
             var list4 = db.Menus.Find(4);
-            model.Url4 = list4 == null ? "" : list4.Media == null ? "" : list4.Media.Url;
+            model.Url4 = list4 == null ? "" : list4.Media == null ? "" : GetCanReturnUrl(list4.Media.Url);
 
             var list5 = db.Menus.Find(5);
-            model.Url5 = list5 == null ? "" : list5.Media == null ? "" : list5.Media.Url;
+            model.Url5 = list5 == null ? "" : list5.Media == null ? "" : GetCanReturnUrl(list5.Media.Url);
 
             return View(model);
+        }
+
+
+        private string GetCanReturnUrl(string url)
+        {
+            if (url.IndexOf("#") >= 0)
+            {
+                var index = url.LastIndexOf("#");
+                return url.Substring(0,index)+ "wechat_redirect";
+            }
+            return $"{url}#wechat_redirect";
         }
 
         public ActionResult About()
         {
             IntroductionViewModel model = new IntroductionViewModel();
             var list1 = db.Menus.Find(6);
-            model.Url1 = list1 == null ? "" : list1.Media == null ? "" : list1.Media.Url;
+            model.Url1 = list1 == null ? "" : list1.Media == null ? "" : GetCanReturnUrl(list1.Media.Url);
             return View(model);
         }
 
@@ -83,6 +94,7 @@ namespace SensingCloud.Controllers
                 model.Name = item.Name;
                 model.MediaId = item.MediaId;
                 model.Media = item.Media;
+                //model.Media.Url = GetCanReturnUrl(model.Media.Url);
                 if (item.Media != null) {
                     var pic_meida = db.Medias.FirstOrDefault(p => p.MediaKey == item.Media.Thumb_MediaId);
                     if (pic_meida != null)
@@ -95,6 +107,12 @@ namespace SensingCloud.Controllers
 
         public ActionResult Contact()
         {
+            return View();
+        }
+
+        public ActionResult WeChatFrame(string url)
+        {
+            ViewBag.Url = url;
             return View();
         }
 
